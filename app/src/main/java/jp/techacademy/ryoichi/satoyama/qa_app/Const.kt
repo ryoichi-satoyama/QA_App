c71be81eefbc08b480e7e8d4293cf1c2cb878792
ryoichi-satoyama <riyoichi_satoyama@mail.toyota.co.jp>
2021-02-13 22:44:01 +0900

    QAアプリ Lesson8 C4完了

---
+++ app/src/main/java/jp/techacademy/ryoichi/satoyama/qa_app/Const.kt
@@ -0,0 +1,9 @@
+package jp.techacademy.ryoichi.satoyama.qa_app
+
+const val UsersPATH = "users"
+
+const val ContentsPATH = "contents"
+
+const val AnswersPATH = "answers"
+
+const val NameKEY = "name"
